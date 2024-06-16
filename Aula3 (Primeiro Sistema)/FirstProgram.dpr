3d8dc8fe76041204523f6e27a97f1cb2d5ac2df1
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-06-16 00:50:30 -0300

    feature: aula 3

---
+++ Aula3 (Primeiro Sistema)/FirstProgram.dpr
@@ -0,0 +1,14 @@
+program FirstProgram;
+
+uses
+  Vcl.Forms,
+  unitMain in 'unitMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ Aula3 (Primeiro Sistema)/unitMain.pas
@@ -0,0 +1,35 @@
+unit unitMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    InputName: TEdit;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+var text: string := InputName.Text;
+
+ShowMessage('Nome: ' + text);
+end;
+
+end.
